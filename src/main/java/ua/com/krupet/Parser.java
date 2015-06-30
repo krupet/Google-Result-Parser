b3079ad2aea21ec8f99a223473aa98a8bd7ec97f
krupet <petrikpjatochkin@gmail.com>
2015-07-01 00:18:34 +0300

    google search page opens fine

--- src/main/java/ua/com/krupet/Parser.java
+++ src/main/java/ua/com/krupet/Parser.java
@@ -1,7 +1,30 @@
 package ua.com.krupet;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by krupet on 30.06.2015.
  */
 public class Parser {
+
+    private static FirefoxDriver driver = new FirefoxDriver();
+    private static String searchUri = "https://www.google.com.ua/";
+
+    public static void main(String[] args) {
+
+        driver.get(searchUri);
+        driver.findElement(By.id("lst-ib")).clear();
+        driver.findElement(By.id("lst-ib")).sendKeys("chuck norris");
+        driver.findElement(By.name("btnG")).click();
+
+        /*
+            here is some bug:
+            INFO: Command failed to close cleanly.
+             Destroying forcefully (v2). org.openqa.selenium.os.UnixProcess$SeleniumWatchDog@78b729e6
+         */
+        driver.quit();
+    }
 }
